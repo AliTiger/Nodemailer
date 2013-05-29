38a3254ea96a6ea40d1036f6fef93c690cf33ec6
刘星 <liuxingmatt@gmail.com>
2013-05-29 16:16:52 +0800

    修改测试代码

--- examples/example_smtp.js
+++ examples/example_smtp.js
@@ -2,14 +2,16 @@ var nodemailer = require('../lib/nodemailer');
 
 // Create a SMTP transport object
 var transport = nodemailer.createTransport("SMTP", {
-        //service: 'Gmail', // use well known service.
-                            // If you are using @gmail.com address, then you don't
-                            // even have to define the service name
-        auth: {
-            user: "test.nodemailer@gmail.com",
-            pass: "Nodemailer123"
-        }
-    });
+    //service: 'Gmail', // use well known service.
+    // If you are using @gmail.com address, then you don't
+    // even have to define the service name
+    host: "10.0.0.117",
+    secureConnection: false,
+    auth: {
+        user: "gokuai@gokuai.office",
+        pass: "office@0"
+    }
+});
 
 console.log('SMTP Configured');
 
@@ -17,10 +19,10 @@ console.log('SMTP Configured');
 var message = {
 
     // sender info
-    from: 'Sender Name <sender@example.com>',
+    from: 'Sender Name <gokuai@gokuai.office>',
 
     // Comma separated list of recipients
-    to: '"Receiver Name" <nodemailer@disposebox.com>',
+    to: '"Receiver Name" <gokuai@gokuai.office>',
 
     // Subject of the message
     subject: 'Nodemailer is unicode friendly ✔', //
